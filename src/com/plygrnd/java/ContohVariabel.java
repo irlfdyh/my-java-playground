b87d32458e49b84256fe65442cf748e3d2443ae8
Your Name <irshalfir@gmail.com>
2021-11-04 13:35:51 +0700

    initial commit

---
+++ src/com/plygrnd/java/ContohVariabel.java
@@ -0,0 +1,11 @@
+package com.plygrnd.java;
+
+public class ContohVariabel {
+    public static void main(String[] args) {
+        String nama;
+        int nilai;
+        nama = "Susi Similikiti";
+        nilai = 100;
+        System.out.println("Nama Saya " + nama + ", Nilai Saya " + nilai);
+    }
+}
